6d2f4a073ff451fbb8c7e5ca3a60a06a59572d43
sergioahp <sergioahp@proton.me>
2026-01-31 02:35:01 -0600

    Add flag lauch with no GPU

--- launch-container.sh
+++ launch-container.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
+
+# Parse arguments
+GPU_FLAG="--gpus all"
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --no-gpu)
+      GPU_FLAG=""
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Usage: $0 [--no-gpu]"
+      exit 1
+      ;;
+  esac
+done
+
 docker run \
   -e LOCAL_UID="$(id -u)" \
   -e LOCAL_GID="$(id -g)" \
@@ -6,5 +23,5 @@ docker run \
   -e GROUP_NAME="$(id -gn)" \
   -v "$PWD:/hf-grpo-tutorial" \
   -w /hf-grpo-tutorial \
-  --gpus all \
+  $GPU_FLAG \
   --rm -it devenv bash
